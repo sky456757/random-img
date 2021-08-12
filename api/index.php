ff54fbdf9379b82ce6129beafc406dedcc5a195f
Thinker <sky456757@gmail.com>
2021-08-12 12:17:01 +0800

    msg

--- api/index.php
+++ api/index.php
@@ -1,13 +1,16 @@
 <?php
 //获取当前绝对路径
-echo "1";
 $path = dirname(__FILE__);
-echo $path;
 //读取文本文件
 $file = file($path."/1.txt");
-echo $file;
 //随机选择一行作为url
 $line = mt_rand(0, count($file)-1);
 $url = trim($file[$line]);
-echo $url;
+if (isset($url))
+{
+Header("HTTP/1.1 303 See Other");
+Header("Location: $url");
+exit;
+}
+?>
 ?>
