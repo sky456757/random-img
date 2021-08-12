d9b14a03bc28b605d87c1a86ab38d607089d2790
Thinker <sky456757@gmail.com>
2021-08-12 12:09:35 +0800

    msg

--- api/index.php
+++ api/index.php
@@ -1,4 +1,13 @@
 <?php
-
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+//获取当前绝对路径
+echo "1";
+$path = dirname(__FILE__);
+echo $path;
+//读取文本文件
+$file = file($path."/1.txt");
+echo $file;
+//随机选择一行作为url
+$line = mt_rand(0, count($file)-1);
+$url = trim($file[$line]);
+echo $url;
+?>
